6241ffbc8cd8685a5a278842eaba1012d8addac8
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-11-27 17:31:15 +0200

    fixing deployment errors

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,4 @@
-import FriendListItem from './components/FriendListItem';
+import FriendListItem from './components/FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 import { List } from './FriendList.styled';
 
